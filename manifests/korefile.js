af22c03ea485dd4b7356a5735d26127cc7b8f44f
Robert Konrad <git2016@robdangero.us>
2018-11-16 22:42:34 +0100

    Almost fix custom commands

--- manifests/korefile.js
+++ manifests/korefile.js
@@ -1,9 +1,10 @@
 let project = new Project('manifests');
 
 if (platform === Platform.Windows) {
-	project.addCustomFile('Microsoft-Scripting-Chakra-Instrumentation.man', 'mc.exe /h "generated\\." /W "$(EventManifestXmlPath)\\winmeta.xml" /w "$(EventManifestXmlPath)\\eventman.xsd" /r "generated\\." /z %(Filename)Events -um /v "%(FullPath)"');
+	project.addCustomFile('Microsoft-Scripting-Chakra-Instrumentation.man',
+		'mc.exe /h "%(RelativeDir)generated\\." /W "$(EventManifestXmlPath)\\winmeta.xml" /w "$(EventManifestXmlPath)\\eventman.xsd" /r "%(RelativeDir)generated\\." /z %(Filename)Events -um /v "%(FullPath)"',
+		'%(RelativeDir)generated\\%(Filename)Events.h;%(RelativeDir)generated\\%(Filename)Events.rc;%(RelativeDir)generated\\%(Filename)Events_MSG00001.bin;%(RelativeDir)generated\\%(Filename)EventsTEMP.bin');
 	project.addIncludeDir('generated');
-	project.addFiles('generated/*');
 }
 
 resolve(project);
